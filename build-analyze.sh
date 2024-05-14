ec8271f98522f721f13ec2d70c8972b0397a5fca
Vincenzo Pellegrini <vincenzo.pellegrini@sonarsource.com>
2024-05-14 14:32:33 +0200

    CPP-5240 Migrate to sonar.cfamily.compile-commands

--- build-analyze.sh
+++ build-analyze.sh
@@ -31,4 +31,4 @@ cmake -B build
 build-wrapper-macosx-x86 --out-dir $BUILD_WRAPPER_OUT_DIR cmake --build build/ --config Release
 
 # Run sonar scanner
-sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL}" -Dsonar.login=$SONAR_TOKEN -Dsonar.cfamily.build-wrapper-output=$BUILD_WRAPPER_OUT_DIR
+sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL}" -Dsonar.login=$SONAR_TOKEN -Dsonar.cfamily.compile-commands=$BUILD_WRAPPER_OUT_DIR/compile_commands.json
